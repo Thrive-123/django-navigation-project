97b9d352e61cbb031e9cfdd7d47e652c3c4c0e71
Thrive-123 <195273530+Thrive-123@users.noreply.github.com>
2026-02-27 09:45:02 +0530

    Moved Procfile to correct root location

--- navigation_project/settings.py
+++ navigation_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fir-dk_*sm8yifrf07320lnnwy8j-)1bxx0f#$x*y**i(np$!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
